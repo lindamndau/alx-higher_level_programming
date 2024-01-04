9dfd2ce0830c55e126ac315cb9c0ee4d58e80722
lindamndau <clavinmndawe@gmail.com>
2024-01-03 19:00:58 -0800

    Higher level programming

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+word = "Holberton"
+print(f"First 3 letters: {word_first_3}", word[0:3])
+print(f"Last 2 letters: {word_last_2}", word[:-2])
+print(f"Middle word: {middle_word}", word[1:-1])
