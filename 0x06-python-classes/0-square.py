7e4d149927a77beaad3becf6ec92b35509fcbc7d
lindamndau <clavinmndawe@gmail.com>
2024-01-24 16:21:03 -0800

    Python Classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    def _init_(self):
-        self.Square = ""
-Square()
+    """ Empty Class"""
+    pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class Square:
+    """Define Class"""
     def __init__(self, size):
+        """ Initialise Instance"""
         self.__size = size
