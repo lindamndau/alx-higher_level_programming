0bcdb2f7fbd38670106a1801ee497209cda79946
lindamndau <clavinmndawe@gmail.com>
2024-02-05 15:39:53 -0800

    Python inheritence

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """ Define lookup class """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class MyList(list):
+    pass
+
+    def print_sorted(self):
+
+        print(sorted(list(self)))
